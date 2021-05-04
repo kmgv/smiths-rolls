53355602823962ace1f2ec2bfcafe8bee106668b
Maciej Sobczak <sobczak.maciej.ms@gmail.com>
2021-05-04 22:03:47 +0200

    add search highlight

--- content_scripts/sort.js
+++ content_scripts/sort.js
@@ -1,3 +1,39 @@
+var regexpReplaceTable = {
+  'a': '[a,ą]',
+  'c': '[c,ć]',
+  'e': '[e,ę]',
+  'l': '[l,ł]',
+  'n': '[n,ń]',
+  'o': '[o,ó]',
+  's': '[s,ś]',
+  'z': '[z,ż,ź]'
+}
+
+var inputCss = `
+  position: absolute;
+  left: 55px;
+  height: 20px;
+  width: 130px;
+`;
+
+function _toStyleString(rule) {
+  return rule.split('\n').filter(function(a){
+    return a.length > 0
+  }).map(function(a){
+    return a.trim()
+  }).join('')
+}
+
+function toRegexp(input) {
+  var regexp = input.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+  for (var letter in regexpReplaceTable) {
+      regexp = regexp.replaceAll(letter, regexpReplaceTable[letter])
+  }
+
+  return new RegExp(regexp, 'i');
+}
+
 (function() {
 
   /**
@@ -18,7 +54,7 @@
         var abilityTr = labelTd.parentElement
 
         var text = labelTd.textContent
-        var sortableText = text.slice(0, text.indexOf('('));
+        var sortableText = text.slice(0, text.lastIndexOf('('));
 
         abilities.push({
           text: sortableText,
@@ -35,7 +71,7 @@
     return _document.querySelector('[data-i18n-list="1920-skill-list"]');
   }
 
-  function sortAbilities(_document){
+  function findAbilitiesTables(_document) {
     var skillList = findSkillList(_document)
 
     if (skillList === null) {
@@ -47,7 +83,16 @@
     var tables = skillList.querySelectorAll('table')
     tables = Array.from(tables).slice(0, 3)
 
-    var abilities = transformAbilitiesTables(tables);
+    return {
+      tables: tables,
+      abilities: transformAbilitiesTables(tables)
+    }
+  }
+
+  function sortSkillList(_document){
+    var abilityTables = findAbilitiesTables(_document);
+    var tables = abilityTables.tables;
+    var abilities = abilityTables.abilities;
 
     var sorted = abilities.sort(function(a, b){
       return a.text.localeCompare(b.text, 'pl', { sensitivity: 'base' })
@@ -68,7 +113,68 @@
     }
   }
 
-  function observeMutationsAndFireSorting() {
+  function insertSearchBar(_document, inputCallback) {
+    var skillHeader = _document.querySelector('[data-i18n="investigatorskills-u"]');
+    if (skillHeader.querySelector('input')){
+      console.log('input found, skipping')
+      return
+    }
+
+    var input = _document.createElement("input");
+    input.setAttribute('style', _toStyleString(inputCss));
+    input.classList.add("smiths-rolls-input");
+    input.addEventListener('input', function(event){
+      var value = event.target.value;
+      inputCallback(value);
+    });
+
+    skillHeader.appendChild(input);
+
+    return input;
+  }
+
+  function initSearchBar(_document) {
+    var abilityTables = findAbilitiesTables(_document);
+    var abilities = abilityTables.abilities;
+
+    var clearHightligh = function() {
+      for (var ability of abilities) {
+        ability.tr.removeAttribute('style');
+      }
+    }
+
+    var input = insertSearchBar(_document, function(searchTerm){
+      if (searchTerm.length == 0){
+        clearHightligh();
+        return
+      }
+
+      var regExp = toRegexp(searchTerm);
+      for (var ability of abilities) {
+        if (ability.text.match(regExp)) {
+          ability.tr.setAttribute('style', 'background: yellow');
+        } else {
+          ability.tr.removeAttribute('style');
+        }
+      }
+    });
+
+    for (var ability of abilities) {
+      for (var node of ability.tr.querySelectorAll('button')) {
+        node.addEventListener('click', function(){
+          input.value = '';
+          clearHightligh();
+        })
+      }
+    }
+  }
+
+  function run(_document){
+    sortSkillList(_document);
+    initSearchBar(_document)
+  }
+
+  function observeMutationsAndRun() {
     var fired = false;
     var debounceTimeout = null;
     var debounceTimeoutTime = 100;
@@ -85,7 +191,7 @@
         debounceTimeout = setTimeout(function(){
           fired = true;
           mutationObserver.disconnect()
-          sortAbilities(document);
+          run(document);
         }, debounceTimeoutTime)
       });
     });
@@ -100,6 +206,6 @@
     });
   }
 
-  observeMutationsAndFireSorting();
+  observeMutationsAndRun();
 
 })();
